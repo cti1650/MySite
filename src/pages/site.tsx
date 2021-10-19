52f222745c11debf47b4746e11ad773804666aef
cti1650 <cti1650@gmail.com>
2021-10-19 10:36:39 +0900

    レイアウト微調整

--- src/pages/site.tsx
+++ src/pages/site.tsx
@@ -80,10 +80,10 @@ const Home = () => {
       </Head>
 
       <div className='w-full text-gray-900 py-8'>
-        <div className="w-full grid grid-cols-1 sm:grid-cols-2 divide-y divide-light-gray-300 text-center content-center justify-center">
+        <div className="w-full grid grid-cols-1 sm:grid-cols-3 divide-y divide-light-gray-300 text-center content-center justify-center">
           {Portfolios.map(item => {
             return (<>
-              <div className="pt-4 w-full flex flex-col space-y-6">
+              <div className="p-4 w-full flex flex-col space-y-6">
                 <div className="w-full content-center "><img src={item.img} alt="image" className="mx-auto max-h-80" /></div>
                 <h2 className="w-full text-4xl tracking-wider text-center text-blue-500 whitespace-pre-wrap">{item.name}</h2>
                 <div className="w-full tracking-wider text-center whitespace-pre-wrap">{item.description}</div>
