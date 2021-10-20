39259be0463dd3fc39cd0f5f8996f221a6fd9503
cti1650 <cti1650@gmail.com>
2021-10-20 09:05:31 +0900

    Update useAge.ts

--- src/hooks/useAge.ts
+++ src/hooks/useAge.ts
@@ -9,7 +9,7 @@ export const useAge = (year,month,date) => {
       };
       const today = new Date();
       const thisYearsBirthday = new Date(today.getFullYear(), birthday.month-1, birthday.date);
-      const userAge = today.getFullYear() - birthday.year;
+      let userAge = today.getFullYear() - birthday.year;
       if(today < thisYearsBirthday){
           userAge--;
       }
