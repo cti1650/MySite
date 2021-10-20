751347637130c3d988ea47c4866878caa9e47d38
cti1650 <cti1650@gmail.com>
2021-10-20 09:00:47 +0900

    Update useAge.ts

--- src/hooks/useAge.ts
+++ src/hooks/useAge.ts
@@ -1,22 +1,19 @@
 import { useMemo, useState, useEffect } from "react";
 
 export const useAge = (year,month,date) => {
-  const [ age,setAge ] = useState(null);
-  const getAge = useMemo(()=>{(birthday){
+  const age = useMemo( () => {
+      const birthday = {
+        year: year,
+        month: month,
+        date: date
+      };
       const today = new Date();
       const thisYearsBirthday = new Date(today.getFullYear(), birthday.month-1, birthday.date);
-      const age = today.getFullYear() - birthday.year;
+      const _age = today.getFullYear() - birthday.year;
       if(today < thisYearsBirthday){
-          age--;
+          _age--;
       }
-      return age;
-  },[]);
-  useEffect(()=>{
-    setAge({
-      year: year,
-      month: month,
-      date: date
-    });
-  },[year,month,date]);
-  return {year,month,dateage};
+      return _age;
+  },[year, month, date]);
+  return {year, month, date, age};
 };
