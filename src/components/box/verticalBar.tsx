8d929932de1df6f5c6ea79027a2a9a3a68a1f28f
cti1650 <cti1650@gmail.com>
2021-10-24 02:19:20 +0900

    💄 VerticalBarコンポーネントのPropsを編集

--- src/components/box/verticalBar.tsx
+++ src/components/box/verticalBar.tsx
@@ -4,12 +4,13 @@ import cc from "classcat";
 type VerticalBarPropsType = {
   children?: any,
   position?: "right" | "left",
+  className?: string,
 }
 
-export const VerticalBar = ({ children, position }: VerticalBarPropsType) => {
+export const VerticalBar = ({ children, position, className }: VerticalBarPropsType) => {
   return (
     <>
-      <div className={cc(["fixed top-0 ", { "right-0": position === 'right', "left-0": position === 'left' }, "w-10 h-full bg-white z-20"])}>
+      <div className={cc(["fixed top-0 ", { "right-0": position === 'right', "left-0": position === 'left' }, "w-10 h-full bg-white z-20", className])}>
         <div className='relative top-1/2 right-0 transform rotate-90 items-center'>
           {children}
         </div>
