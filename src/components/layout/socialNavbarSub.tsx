1a7b1c7e54d31b05367d93d12dad268a47c4c7f5
cti1650 <cti1650@gmail.com>
2021-10-30 16:34:12 +0900

    ✨ ZENNを追加

--- src/components/layout/socialNavbarSub.tsx
+++ src/components/layout/socialNavbarSub.tsx
@@ -9,6 +9,7 @@ export default function SocialNav({ children }) {
         <ol className='h-full w-full py-2 sm:py-4 flex justify-center items-center content-center space-x-6 text-gray-800'>
           <li><SocialButton href='https://github.com/cti1650'>GITHUB</SocialButton></li>
           <li><SocialButton href='https://www.wantedly.com/id/yuichi_sakagami'>WANTEDLY</SocialButton></li>
+          <li><SocialButton href='https://zenn.dev/cti1650'>ZENN</SocialButton></li>
         </ol>
       </VerticalBar>
     </>
