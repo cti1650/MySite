830134faedaed11a89f8c79d53c49a2bf18b5b18
cti1650 <cti1650@gmail.com>
2021-11-02 14:18:48 +0900

    🐛 cors対応

--- src/pages/api/notion/index.js
+++ src/pages/api/notion/index.js
@@ -1,6 +1,27 @@
 import { useNotionClient } from '@hooks/useNotion';
+import Cors from 'cors';
+
+// Initializing the cors middleware
+const cors = Cors({
+  methods: ['POST', 'OPTION', 'HEAD'],
+});
+
+// Helper method to wait for a middleware to execute before continuing
+// And to throw an error when an error happens in a middleware
+function runMiddleware(req, res, fn) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result) => {
+      if (result instanceof Error) {
+        return reject(result);
+      }
+
+      return resolve(result);
+    });
+  });
+}
 
 export default async function handler(req, res) {
+  await runMiddleware(req, res, cors);
   // console.log(
   //   `key:${process.env.NEXT_PUBLIC_NOTION_KEY} id:${process.env.NEXT_PUBLIC_NOTION_DATABASE_ID}`
   // );
