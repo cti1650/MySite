606caa24e463a3ae84558276219bf3691dfa2729
cti1650 <cti1650@gmail.com>
2022-05-20 21:05:34 +0900

    💄 feat: 共通レイアウトの変更

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,21 +1,18 @@
 import React from 'react';
 import { NextPage } from 'next';
 import Head from 'next/head';
-import {
-  Container
-} from '@material-ui/core';
 import { MantineForm } from '@comp/form/mantineForm';
 
 const Contact: NextPage = () => {
   return (
-    <div className="w-full px-10 font-n2i">
+    <div className="w-full h-full font-n2i">
       <Head>
         <title>cti1650 Portfolio</title>
         <meta property="og:title" content="cti1650 Portfolio" />
       </Head>
-      <Container maxWidth="xs" className="pt-[30px]">
+      <div className='h-full w-full flex justify-center items-center px-4 font-n2i'>
         <MantineForm />
-      </Container>
+      </div>
     </div>
   );
 };
