9dd3edbd12a68e304ffa12049f5fd153658b56cc
cti1650 <cti1650@gmail.com>
2022-05-23 01:35:58 +0900

    feat: パッケージバージョンアップ

--- src/components/layout/layoutSub.tsx
+++ src/components/layout/layoutSub.tsx
@@ -4,20 +4,18 @@ import React, { VFC } from 'react';
 import { SocialNav } from './socialNavbarSub';
 
 type Props = {
-  children: React.ReactNode;
+  children?: React.ReactNode;
 };
 
 export const Layout: VFC<Props> = ({ children }) => {
   return (
-    <>
-      <div className="h-screen w-full mx-0 font-n2i">
-        <Navbar />
-        <main className="h-full pt-[90px]">
-          <Footer />
-          <div className="min-h-full h-full px-[40px]"><div className="min-h-full h-full container mx-auto">{children}</div></div>
-          <SocialNav />
-        </main>
-      </div>
-    </>
+    <div className="h-screen w-full mx-0 font-n2i">
+      <Navbar />
+      <main className="h-full pt-[90px]">
+        <Footer />
+        <div className="min-h-full h-full px-[40px]"><div className="min-h-full h-full container mx-auto">{children}</div></div>
+        <SocialNav />
+      </main>
+    </div>
   );
 };
