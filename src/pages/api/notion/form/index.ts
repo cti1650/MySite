9709d3acca66b1bcb637f261650b6e459e3a921d
cti1650 <cti1650@gmail.com>
2024-04-03 11:06:47 +0000

    feat: update form

--- src/pages/api/notion/form/index.ts
+++ src/pages/api/notion/form/index.ts
@@ -6,10 +6,32 @@ type ResponseData = {
   error: string;
 };
 
+import Cors from 'cors';
+
+// CORS のミドルウェアを初期化
+const cors = Cors({
+  methods: ['GET', 'POST', 'HEAD', 'OPTIONS'],
+});
+
+// 後続の処理を行う前にミドルウェアの実行を待ち、
+// また、ミドルウェアでエラーが発生したときエラーを投げるためのヘルパーメソッド
+function runMiddleware(req, res, fn) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result) => {
+      if (result instanceof Error) {
+        return reject(result);
+      }
+
+      return resolve(result);
+    });
+  });
+}
+
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Partial<ResponseData>>
 ) {
+  await runMiddleware(req, res, cors);
   const endpoint = process.env.NEXT_PUBLIC_NOTION_BACKEND_ENDPOINT;
   const apiKey = process.env.NEXT_PUBLIC_NOTION_KEY;
   const databaseId = process.env.NEXT_PUBLIC_NOTION_CONTACT_DATABASE_ID;
