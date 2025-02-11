1b560182203dc7f1dd01fe9e55a3fe77f3a9d680
cti1650 <cti1650@gmail.com>
2025-02-11 22:34:30 +0900

    fix: update lnk

--- src/pages/lnk/[shortcut_name].tsx
+++ src/pages/lnk/[shortcut_name].tsx
@@ -1,23 +1,23 @@
-import { GetServerSideProps, NextPage } from 'next'
+import { GetServerSideProps, NextPage } from 'next';
 
-type Props = {}
+type Props = {};
 
 const ShortcutRedirect: NextPage<Props> = () => {
-  return null
+  return null;
 }
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
-  const { shortcut_name } = context.params as { shortcut_name: string }
+  const { shortcut_name } = context.params as { shortcut_name: string };
 
   const redirectMap: Record<string, string> = {
     playground: 'https://paiza.io/ja/projects/new',
     space: 'https://app.rc.ovice.com/ws/p4ofxd8oeo/'
-  }
+  };
 
   if (!redirectMap[shortcut_name]) {
     return {
       notFound: true,
-    }
+    };
   }
 
   return {
@@ -25,7 +25,7 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
       destination: redirectMap[shortcut_name],
       permanent: false,
     },
-  }
+  };
 }
 
-export default ShortcutRedirect
+export default ShortcutRedirect;
