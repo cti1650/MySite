81b62113ae7a678ec6394d7660a64835adab6381
cti1650 <cti1650@gmail.com>
2025-02-22 04:16:31 +0900

    Update [shortcut_name].tsx

--- src/pages/lnk/[shortcut_name].tsx
+++ src/pages/lnk/[shortcut_name].tsx
@@ -12,6 +12,7 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
   const redirectMap: Record<string, string> = {
     playground: 'https://paiza.io/ja/projects/new',
     space: 'https://app.rc.ovice.com/ws/p4ofxd8oeo/',
+    chrome_extension_install: 'https://scrapbox.io/cti-scrap/Chrome%E6%8B%A1%E5%BC%B5%E6%A9%9F%E8%83%BD%E3%81%AE%E3%83%AD%E3%83%BC%E3%82%AB%E3%83%AB%E3%82%A4%E3%83%B3%E3%82%B9%E3%83%88%E3%83%BC%E3%83%AB%E6%96%B9%E6%B3%95'
   };
 
   if (!redirectMap[shortcut_name]) {
