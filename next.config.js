55d03982f07f39263afb0027a8d021e8f759adf3
cti1650 <cti1650@gmail.com>
2025-06-14 17:05:26 +0900

    feat: llmsのAPIアクセス制限を緩和

--- next.config.js
+++ next.config.js
@@ -34,6 +34,23 @@ const nextConfig = {
           },
         ],
       },
+      {
+        source: '/api/llms/:path*',
+        headers: [
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: '*',
+          },
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET,OPTIONS,POST',
+          },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value: 'Content-Type',
+          },
+        ],
+      },
     ];
   },
   async rewrites() {
